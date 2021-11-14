7737b0cb317d09f9ff44db7523a89e7d480b596a
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-11-14 10:54:22 +0530

    Added data fetch and home page with results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [candidates, setCandidates] = useState([]);
+	useEffect(() => {
+		fetch(
+			"https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json"
+		)
+			.then((response) => response.json())
+			.then((data) => setCandidates(data));
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Candidate Screener</h1>
+			<div className="candidates-list">
+				{candidates.map((candidate) => (
+					<div className="card-container" key={candidate.id}>
+						<img src={candidate.Image} alt="" />
+						<h2>{candidate.name}</h2>
+						<p>{candidate.id}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
